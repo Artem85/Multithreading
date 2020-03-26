e7b3a2057b9c7c7aac0cb583d69c203ca19c5b9c
johnymnemonic <zainchkovskyy.artem@gmail.com>
2020-03-26 13:23:07 +0200

    ParallelExecution completed

---
+++ ParallelExecution/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ParallelExecution
+{
+    class Program
+    {
+        static int totalAndCount;
+
+        static void Main(string[] args)
+        {
+            string projectDir = Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory()));
+            string folderName = $@"{projectDir}/ParallelExecTest";
+
+            var files = Directory.GetFiles(folderName);
+
+            //Parallel.Foreach
+            int totalAndCntForEach = 0;
+            Parallel.ForEach<string>(files, (file =>
+            {
+                int andCount = 0;
+
+                string text = File.ReadAllText(file);
+                var andMatches = Regex.Matches(text, "and");
+                andCount = andMatches.Count;
+
+                Console.WriteLine($"File {file} contains {andCount} 'and'");
+                totalAndCntForEach += andCount;
+            }));
+            Console.WriteLine($"Total 'and' count is {totalAndCntForEach}");
+
+            //PLINQ
+            var processedFiles = files
+                                .AsParallel();
+
+            foreach (var processedFile in processedFiles)
+            {
+                GetAndCount(processedFile);
+            }
+
+            Console.WriteLine($"Total 'and' count is {totalAndCount}");
+            Console.ReadKey();
+        }
+
+        private static void GetAndCount(string file)
+        {
+            int andCount = 0;
+
+            string text = File.ReadAllText(file);
+            var andMatches = Regex.Matches(text, "and");
+            andCount = andMatches.Count;
+
+            Console.WriteLine($"File {file} contains {andCount} 'and'");
+            totalAndCount += andCount;
+        }
+    }
+}
---
+++ Synchronization/D.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Synchronization
+{
+    public class D
+    {
+        static List<int> col = new List<int>();
+        static AutoResetEvent waitHandler = new AutoResetEvent(true);
+
+        public static void Test()
+        {
+            var thread1 = new Thread(Add5Elements);
+            var thread2 = new Thread(Add5Elements);
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+
+            Console.WriteLine($"col.Count = {col.Count}");
+
+            Console.WriteLine("Press Enter to Continue");
+            Console.ReadKey();
+        }
+
+        private static void Add5Elements()
+        {
+            waitHandler.WaitOne();
+
+            for (int i = 0; i < 5; i++)
+            {
+                int item = Thread.CurrentThread.ManagedThreadId + i;
+                col.Add(item);
+                Console.WriteLine($"Add item {item}");
+            }
+
+            waitHandler.Set();
+        }
+    }
+}
